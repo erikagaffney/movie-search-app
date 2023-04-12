0a6dc7e5484490a44cb09ad45a6649a8c21ff6fc
erikagaffney <erikagaffney.2014@gmail.com>
2023-04-12 18:35:10 -0500

    new tests & destructuring movies object for easier variables (#19)

---
+++ src/components/ErrorAlert.test.js
@@ -0,0 +1,43 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import ErrorAlert from './ErrorAlert';
+
+test('renders when show alert is true', async () => {
+  // ARRANGE
+  const showAlert = true;
+  const { container } = render(
+    <ErrorAlert showAlert={showAlert} setShowAlert={jest.fn()} />
+  );
+
+  // ACT
+  const snackbar = await screen.findByRole('presentation');
+
+  // ASSERT
+  expect(snackbar).toBeInTheDocument();
+  expect(container).not.toBeEmptyDOMElement();
+});
+
+test('renders empty container when show alert is false', () => {
+  // ARRANGE
+  const showAlert = false;
+  const { container } = render(
+    <ErrorAlert showAlert={showAlert} setShowAlert={jest.fn()} />
+  );
+
+  // ASSERT
+  expect(container).toBeEmptyDOMElement();
+});
+
+test('should call set show alert with false when the close button is clicked', async () => {
+  // ARRANGE
+  const showAlert = true;
+  const mock = jest.fn();
+  render(<ErrorAlert showAlert={showAlert} setShowAlert={mock} />);
+
+  // Act
+  const button = await screen.findByRole('button', { hidden: true });
+  await userEvent.click(button);
+
+  // ASSERT
+  expect(mock).toHaveBeenCalledWith(false);
+});
